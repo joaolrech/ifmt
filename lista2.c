db85f9e577bbee8a4a113f94eb66bb86fe06228e
joaolrech <158629556+joaolrech@users.noreply.github.com>
2024-02-17 20:10:23 +0000

    16/02/2024

--- lista2.c
+++ lista2.c
@@ -208,29 +208,17 @@ void q10()
     scanf("%d", &c);
 
     if(a <= b && b <= c)
-    {
         printf("%d, %d, %d \n", a, b, c);
-    }
     else if(a <= c && c <= b)
-    {
         printf("%d, %d, %d \n", a, c, b);
-    }
     else if(b <= a && a <= c)
-    {
         printf("%d, %d, %d \n", b, a, c);
-    }
     else if(b <= c && c <= a)
-    {
         printf("%d, %d, %d \n", b, c, a);
-    }
     else if(c <= a && a <= b)
-    {
         printf("%d, %d, %d \n", c, a, b);
-    }
     else if(c <= b && b <= a)
-    {
         printf("%d, %d, %d \n", c, b, a);
-    }
 }
 
 //11. Faça um programa que leia 3 números e imprima o maior deles.
