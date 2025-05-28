c54eef2a7c1468b1cffb8480e7898b6c40d6c7c3
joaolrech <158629556+joaolrech@users.noreply.github.com>
2025-05-28 22:14:31 +0000

    28/05/2025

--- circuitos.py
+++ circuitos.py
@@ -1,9 +1,42 @@
-matriz = [
-    ['R', 1, 1, 2, 3],
-    ['R', 2, 0, 2, 2],
-    ['R', 3, 2, 3, 3],
-    ['R', 4, 0, 3, 2],
-    ['V', 1, 0, 1, -17],
-    ['V', 2, 2, 3, 6],
-    ['I', 1, 0, 3, 2]
-]
+import os
+
+def soma(x, y):
+    pass
+    
+def subtracao(x, y):
+    pass
+    
+def multiplicacao(x, y):
+    pass
+    
+def divisao(x, y):
+    pass
+    
+def inpt():
+    while True:
+        try:
+            print(
+                'Selecione a associação do circuito.\n'
+                '1 - R\n'
+                '2 - L\n'
+                '3 - C\n'
+                '4 - R+L\n'
+                '5 - R+C\n'
+                '6 - L+C\n'
+                '7 - R+L+C\n'
+                '8 - R//L\n'
+                '9 - R//C\n'
+                '10 - L//C\n'
+                '11 - R//L//C'
+            )
+            inpt = int(input())
+            if inpt < 1 or inpt > 11:
+                raise ValueError
+            return inpt
+        except ValueError:
+            os.system('clear')
+            print('Entrada inválida. Tente novamente.\n')
+
+os.system('clear')
+print('Bem vindo ao simulador de circuitos CA!\n')
+associacao = inpt()
